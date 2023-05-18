599ed96388c505ca9462f0e21651927dde3437e0
HoVDuc <hoduccity@gmail.com>
2023-05-18 10:14:49 +0000

    add dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,32 @@
+from torch.utils.data import Dataset
+import torch
+from PIL import Image
+
+
+class MedicalData(Dataset):
+
+    def __init__(self, path, df, device, transform=None):
+        self.df = df.to_numpy()
+        self.convert_labels(df)
+        self.device = device
+        self.path = path
+        self.transform = transform
+
+    def convert_labels(self, df):
+        labels = set(df['label'])
+        self.class2index = {label: idx for idx, label in enumerate(labels)}
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        filename = self.df[index][0]
+        folder = self.df[index][1]
+        label = torch.tensor(self.class2index[folder], device=self.device)
+        file_path = f"{self.path}{folder}/{filename}"
+        image = Image.open(file_path)
+        if self.transform is not None:
+            image = self.transform(image)
+        image = image.to(self.device)
+        label = label.to(self.device)
+        return image, label

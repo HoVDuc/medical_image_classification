46bdac26737678e41aa34e939b01bf28c6041af1
HoVDuc <hoduccity@gmail.com>
2023-05-23 14:47:56 +0700

    update trainer.py

--- trainer.py
+++ trainer.py
@@ -6,13 +6,16 @@ from tqdm import tqdm
 from loss.focal_loss import FocalLoss
 from torchmetrics.classification import MulticlassPrecision, MulticlassRecall, MulticlassConfusionMatrix
 from torch.utils.tensorboard import SummaryWriter
+import pandas as pd
+from sklearn.metrics import classification_report
 
 
 class Trainer:
 
-    def __init__(self, model, loss, epochs, max_lr, device, num_samples, kfold=1, print_every=5) -> None:
+    def __init__(self, model, num_classes, loss, epochs, max_lr, device, num_samples, kfold=1, print_every=5) -> None:
         self.device = device
         self.model = model.to(self.device)
+        self.num_classes = num_classes
         self.epochs = epochs
         self.lr = max_lr
         self.print_every = print_every
@@ -53,67 +56,80 @@ class Trainer:
     def f1_scores(self, preds, targets):
         preds = torch.argmax(preds, dim=1)
         f1 = torchmetrics.F1Score(
-            task='multiclass', num_classes=6).to(self.device)
+            task='multiclass', num_classes=self.num_classes).to(self.device)
         return f1(preds, targets)
 
     def precision(self, preds, targets):
-        metric = MulticlassPrecision(num_classes=6).to(self.device)
+        metric = MulticlassPrecision(num_classes=self.num_classes).to(self.device)
         return metric(preds, targets)
 
     def recall(self, preds, targets):
-        metric = MulticlassRecall(num_classes=6).to(self.device)
+        metric = MulticlassRecall(num_classes=self.num_classes).to(self.device)
         return metric(preds, targets)
 
     def mean_average_precision(self, preds, targets):
         average_precision = torchmetrics.AveragePrecision(
-            task="multiclass", num_classes=6, average="macro", thresholds=5).to(self.device)
+            task="multiclass", num_classes=self.num_classes, average="macro", thresholds=5).to(self.device)
         ap = average_precision(preds, targets)
         return ap
 
     def confusion_matrix(self, preds, targets):
-        metric = MulticlassConfusionMatrix(num_classes=6).to(self.device)
+        metric = MulticlassConfusionMatrix(num_classes=self.num_classes).to(self.device)
         return metric(preds, targets)
+    
+    def metrics(self, pred, targets):
+        f1_scores = self.f1_scores(pred, targets)
+        mAP = self.mean_average_precision(pred, targets)
+        precision = self.precision(pred, targets)
+        recall = self.recall(pred, targets)
+        confusion_matrix = self.confusion_matrix(pred, targets)
+        return f1_scores, mAP, precision, recall, confusion_matrix
+    
+    def display_classification_report(preds, targets):
+        targets_name = pd.read_csv('./class.csv')['class'].to_list()
+        print(classification_report(targets, preds, target_names=targets_name))
+    
+    def validation_step(self, batch):
+        self.model.eval()
+        inputs, targets = batch
+        pred = self.model(inputs)
+        loss = self.criterion(pred, targets)
+        
+        return loss.item(), pred, targets
+    
 
     def validation(self, mode='val'):
-        self.model.eval()
-        total_loss = 0
-        # total_accuracy = 0
-        total_f1_scores = 0
-        total_map = 0
-        total_precision = 0
-        total_recall = 0
-        total_cf = torch.zeros(torch.Size([6, 6]), device=self.device)
+        
+        total_metrics = {
+            'loss': 0,
+            'f1_scores': 0,
+            'precision': 0,
+            'mAP': 0,
+            'recall': 0
+        }
+        total_cf = torch.zeros(torch.Size([self.num_classes, self.num_classes]), device=self.device)
+        preds, targets = [], []
 
         data_loader = self.valid_loader if mode == 'val' else self.test_loader
         with torch.no_grad():
             for batch in data_loader:
-                inputs, targets = batch
-                pred = self.model(inputs)
-                loss = self.criterion(pred, targets)
-                total_loss += loss
-                # calc metrics
-                # accuracy = self.calc_accuracy(pred, targets)
-                f1_scores = self.f1_scores(pred, targets)
-                mAP = self.mean_average_precision(pred, targets)
-                precision = self.precision(pred, targets)
-                recall = self.recall(pred, targets)
-                confusion_matrix = self.confusion_matrix(pred, targets)
-                # calc total metrics
-                # total_accuracy += accuracy
-                total_f1_scores += f1_scores
-                total_map += mAP
-                total_precision += precision
-                total_recall += recall
-                total_cf += confusion_matrix
+                loss, pred, target = self.validation_step(batch)
+                preds.append(pred)
+                targets.append(target)
+            
+            preds = torch.tensor(preds).flatten()
+            targets = torch.tensor(targets).flatten()
+            self.display_classification_report(preds, targets)
+            f1_scores, mAP, precision, recall, confusion_matrix = self.metrics(preds, targets)
+            total_metrics['loss'] += loss
+            total_metrics['f1_scores'] += f1_scores
+            total_metrics['precision'] += precision
+            total_metrics['mAP'] += mAP
+            total_metrics['recall'] += recall
+            total_cf += confusion_matrix
 
-            avg_loss = total_loss / len(data_loader)
-            # avg_accuracy = total_accuracy / len(data_loader)
-            avg_f1_scores = total_f1_scores / len(data_loader)
-            avg_map = total_map / len(data_loader)
-            avg_precision = total_precision / len(data_loader)
-            avg_recall = total_recall / len(data_loader)
 
-        return avg_loss, avg_f1_scores, avg_map, avg_precision, avg_recall, total_cf
+        return total_metrics, total_cf
 
     def save_model(self, save_path):
         torch.save(self.model.state_dict(), save_path)
@@ -142,16 +158,14 @@ class Trainer:
             self.writer.add_scalar('Training loss', avg_loss, global_step=step)
             step += 1
             if epoch % self.print_every == 0:
-                avg_val_loss, avg_val_f1_scores, avg_val_map, avg_val_precision, avg_val_recall, cf = self.validation(
+                avg_val_metrics, cf = self.validation(
                     mode='val')
 
-                print("loss: {} - val_loss: {} - f1_scores: {} - mAP: {} - precision: {} - recall: {}".format(
-                    avg_loss, avg_val_loss, avg_val_f1_scores, avg_val_map, avg_val_precision, avg_val_recall))
+                print('Avg loss:', avg_loss)
+                print('metrics:', avg_val_metrics)
                 print('Confusion Matrix:\n', cf)
 
         if self.test_loader:
-            avg_test_loss, avg_test_f1_scores, avg_test_map, avg_test_precision, avg_test_recall, cf = self.validation(
-                mode='test')
-            print("test_loss: {} - f1_scores: {} - mAP: {} - precision: {}: recall: {}".format(
-                avg_test_loss, avg_test_f1_scores, avg_test_map, avg_test_precision, avg_test_recall))
+            avg_test_metrics, cf = self.validation(mode='test')
+            print('metrics test:', avg_test_metrics)
             print('Confusion Matrix:\n', cf)

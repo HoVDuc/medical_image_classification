cf143d4d05e836635f5c6108b36d5e229ad3166a
HoVDuc <hoduccity@gmail.com>
2023-06-17 21:17:43 +0700

    albumentations used to image augmentation

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,8 @@
 from torch.utils.data import Dataset
 import torch
+from torchvision import transforms 
 from PIL import Image
+import numpy as np
 
 
 class MedicalData(Dataset):
@@ -25,8 +27,14 @@ class MedicalData(Dataset):
         label = torch.tensor(self.class2index[folder], device=self.device)
         file_path = f"{self.path}{folder}/{filename}"
         image = Image.open(file_path)
-        if self.transform is not None:
-            image = self.transform(image)
+        image = np.array(image)
+        to_tensor = transforms.Compose([
+            transforms.ToTensor(), 
+        ])
+        if self.transform:
+            image = self.transform(image=image)['image']
+            image = to_tensor(image)
+            
         image = image.to(self.device)
         label = label.to(self.device)
         return image, label
